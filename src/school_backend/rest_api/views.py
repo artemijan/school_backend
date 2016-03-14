19663be880aa3d7f61aa9ae26e02d58527ea871d
artem <lineartem@gmail.com>
2016-03-14 18:38:39 +0200

    disable post request by url:'/users/', because user creation is "registration" action. So it's related to another business logic

--- src/school_backend/rest_api/views.py
+++ src/school_backend/rest_api/views.py
@@ -1,14 +1,20 @@
 from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
+from rest_framework import mixins, viewsets
 from rest_api.serializers import UserSerializer, GroupSerializer
+from rest_framework import permissions
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserViewSet(mixins.RetrieveModelMixin,
+                  mixins.UpdateModelMixin,
+                  mixins.DestroyModelMixin,
+                  mixins.ListModelMixin,
+                  viewsets.GenericViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
+    permission_classes = (permissions.IsAuthenticated,)
 
 
 class GroupViewSet(viewsets.ModelViewSet):
